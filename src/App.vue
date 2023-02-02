f386ef8d0b84a17cc63b736c2be9001495cdeb4a
huanghw <huanghw@sipsd.local>
2023-02-02 10:08:12 +0800

    feat✨:  slot

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <HelloWorld msg="Welcome to Your Vue.js App"><h3>大标题</h3></HelloWorld>
+    <!--如果HelloWorld中的没有slot元素，那么<h3>大标题</h3>将不会显示-->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>

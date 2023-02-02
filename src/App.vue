4393b73c3231f639da5801c22622ef171bb99f32
huanghw <huanghw@sipsd.local>
2023-02-02 10:30:22 +0800

    重写slot组件

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-    <HelloWorld msg="Welcome to Your Vue.js App"><h3>大标题</h3></HelloWorld>
+    <child-component msg="Welcome to Your Vue.js App"
+      ><h3>大标题</h3></child-component
+    >
     <!--如果HelloWorld中的没有slot元素，那么<h3>大标题</h3>将不会显示-->
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import ChildComponent from "./components/ChildComponent.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    ChildComponent,
   },
 };
 </script>
